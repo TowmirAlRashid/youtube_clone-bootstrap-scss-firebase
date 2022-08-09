95e5f6c92cc0bc594badf3f9f708eade4d8aba03
AL RASHID TOWMIR <alrashidtowmir420@gmail.com>
2022-08-09 20:53:37 +0600

    added Categories Bar UI

--- src/components/catagoriesBar/CatagoriesBar.jsx
+++ src/components/catagoriesBar/CatagoriesBar.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
+import { useState } from 'react';
 import './_catagoriesBar.scss'
 
+const keywords = [
+  "All",
+  "React js",
+  "JavaScript",
+  "React Native",
+  "use of API",
+  "Redux",
+  "Shoddo Khan",
+  "Algorithm Art",
+  "Guitar",
+  "Bengali Songs",
+  "Coding",
+  "Cricket",
+  "Football",
+  "Real Madrid",
+  "Christiano Ronaldo",
+  "Poor Coder",
+  "Shwetabh",
+];
+
 const CatagoriesBar = () => {
+  const [activeElement, setActiveElement] = useState('all')
+
+  const handleClick = (value) => {
+    setActiveElement(value)
+  }
+
   return (
-    <div>CatagoriesBar</div>
+    <div className='categoriesBar'>
+      {
+        keywords.map((keyValue, index) => (
+          <span 
+            key={index}
+            onClick={() => handleClick(keyValue)}
+            className={activeElement === keyValue ? 'active' : ''}
+          >
+            {keyValue}
+          </span>
+        ))
+      }
+    </div>
   )
 }
 
