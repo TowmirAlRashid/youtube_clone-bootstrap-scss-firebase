9620680a4f29e55ee034dbe62b6c4bc60420a4c8
AL RASHID TOWMIR <alrashidtowmir420@gmail.com>
2022-08-10 13:05:11 +0600

    set up Router for the App

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import Header from './components/header/Header'
 import Sidebar from './components/sidebar/Sidebar'
 import HomeScreen from './components/screens/HomeScreen/HomeScreen'
 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
 import './_app.scss'
 import LoginScreen from './components/screens/loginScreen/LoginScreen'
 
-const App = () => {
+const Layout = ({children}) => {
   const [sidebar, toggleSidebar] = useState(false)
 
   const handleToggleSidebar = () => toggleSidebar((value) => !value)
-  
+
   return (
     <div>
       <Header handleToggleSidebar={handleToggleSidebar} />
@@ -19,11 +21,51 @@ const App = () => {
         <Sidebar sidebar={sidebar} handleToggleSidebar={handleToggleSidebar} />
 
         <Container fluid className="app__main">
-          <HomeScreen />
+          {children}
         </Container>
       </div>
     </div>
-    // <LoginScreen />
+  )
+}
+
+const App = () => {
+  
+  
+  return (
+    <Router>
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={
+            <Layout>
+              <HomeScreen />
+            </Layout>
+          }
+        />
+
+        <Route exact path="/auth" element={<LoginScreen />} />
+
+        <Route
+          exact
+          path="/search"
+          element={
+            <Layout>
+              <h1>Search Results</h1>
+            </Layout>
+          }
+        />
+
+        <Route
+          path="*"
+          element={
+            <Layout>
+              <HomeScreen />
+            </Layout>
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
